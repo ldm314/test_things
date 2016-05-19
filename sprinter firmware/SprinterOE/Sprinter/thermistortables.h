1f418254e46c2f355a575826423836d2ff890bf8
Brian Moore <brian@forgottenfords.org>
2016-05-19 14:09:58 -0700

    Add files via upload

---
+++ sprinter firmware/SprinterOE/Sprinter/thermistortables.h
@@ -0,0 +1,254 @@
+ #ifndef THERMISTORTABLES_H_
+#define THERMISTORTABLES_H_
+
+#if (THERMISTORHEATER == 1) || (THERMISTORBED == 1) //100k bed thermistor
+
+
+#define NUMTEMPS_1 61
+const short temptable_1[NUMTEMPS_1][2] = {
+{	23	,	300	},
+{	25	,	295	},
+{	27	,	290	},
+{	28	,	285	},
+{	31	,	280	},
+{	33	,	275	},
+{	35	,	270	},
+{	38	,	265	},
+{	41	,	260	},
+{	44	,	255	},
+{	48	,	250	},
+{	52	,	245	},
+{	56	,	240	},
+{	61	,	235	},
+{	66	,	230	},
+{	71	,	225	},
+{	78	,	220	},
+{	84	,	215	},
+{	92	,	210	},
+{	100	,	205	},
+{	109	,	200	},
+{	120	,	195	},
+{	131	,	190	},
+{	143	,	185	},
+{	156	,	180	},
+{	171	,	175	},
+{	187	,	170	},
+{	205	,	165	},
+{	224	,	160	},
+{	245	,	155	},
+{	268	,	150	},
+{	293	,	145	},
+{	320	,	140	},
+{	348	,	135	},
+{	379	,	130	},
+{	411	,	125	},
+{	445	,	120	},
+{	480	,	115	},
+{	516	,	110	},
+{	553	,	105	},
+{	591	,	100	},
+{	628	,	95	},
+{	665	,	90	},
+{	702	,	85	},
+{	737	,	80	},
+{	770	,	75	},
+{	801	,	70	},
+{	830	,	65	},
+{	857	,	60	},
+{	881	,	55	},
+{	903	,	50	},
+{	922	,	45	},
+{	939	,	40	},
+{	954	,	35	},
+{	966	,	30	},
+{	977	,	25	},
+{	985	,	20	},
+{	993	,	15	},
+{	999	,	10	},
+{	1004	,	5	},
+{	1008	,	0	} //safety
+};
+#endif
+#if (THERMISTORHEATER == 2) || (THERMISTORBED == 2) //200k bed thermistor
+#define NUMTEMPS_2 21
+const short temptable_2[NUMTEMPS_2][2] = {
+   {1, 848},
+   {54, 275},
+   {107, 228},
+   {160, 202},
+   {213, 185},
+   {266, 171},
+   {319, 160},
+   {372, 150},
+   {425, 141},
+   {478, 133},
+   {531, 125},
+   {584, 118},
+   {637, 110},
+   {690, 103},
+   {743, 95},
+   {796, 86},
+   {849, 77},
+   {902, 65},
+   {955, 49},
+   {1008, 17},
+   {1020, 0} //safety
+};
+
+#endif
+#if (THERMISTORHEATER == 3) || (THERMISTORBED == 3) //mendel-parts
+#define NUMTEMPS_3 28
+const short temptable_3[NUMTEMPS_3][2] = {
+		{1,864},
+		{21,300},
+		{25,290},
+		{29,280},
+		{33,270},
+		{39,260},
+		{46,250},
+		{54,240},
+		{64,230},
+		{75,220},
+		{90,210},
+		{107,200},
+		{128,190},
+		{154,180},
+		{184,170},
+		{221,160},
+		{265,150},
+		{316,140},
+		{375,130},
+		{441,120},
+		{513,110},
+		{588,100},
+		{734,80},
+		{856,60},
+		{938,40},
+		{986,20},
+		{1008,0},
+		{1018,-20}
+	};
+
+#endif
+#if (THERMISTORHEATER == 4) || (THERMISTORBED == 4) //10k thermistor
+
+#define NUMTEMPS_4 20
+short temptable_4[NUMTEMPS_4][2] = {
+   {1, 430},
+   {54, 137},
+   {107, 107},
+   {160, 91},
+   {213, 80},
+   {266, 71},
+   {319, 64},
+   {372, 57},
+   {425, 51},
+   {478, 46},
+   {531, 41},
+   {584, 35},
+   {637, 30},
+   {690, 25},
+   {743, 20},
+   {796, 14},
+   {849, 7},
+   {902, 0},
+   {955, -11},
+   {1008, -35}
+};
+#endif
+
+#if (THERMISTORHEATER == 5) || (THERMISTORBED == 5) //100k thermistor
+
+#define NUMTEMPS_5 52
+short temptable_5[NUMTEMPS_5][2] = {
+         {1,        938},
+         {31,       314},
+         {41,       290},
+         {51,       272},
+         {61,       258},
+         {71,       247},
+         {81,       237},
+         {91,       229},
+         {101,      221},
+         {111,      215},
+         {121,      209},
+         {131,      204},
+         {141,      199},
+         {151,      195},
+         {161,      190},
+         {171,      187},
+         {181,      183},
+         {191,      179},
+         {201,      176},
+         {221,      170},
+         {241,      165},
+         {261,      160},
+         {281,      155},
+         {301,      150},
+         {331,      144},
+         {361,      139},
+         {391,      133},
+         {421,      128},
+         {451,      123},
+         {491,      117},
+         {531,      111},
+         {571,      105},
+         {611,      100},
+         {641,      95},
+         {681,      90},
+         {711,      85},
+         {751,      79},
+         {791,      72},
+         {811,      69},
+         {831,      65},
+         {871,      57},
+         {881,      55},
+         {901,      51},
+         {921,      45},
+         {941,      39},
+         {971,      28},
+         {981,      23},
+         {991,      17},
+         {1001,     9},
+         {1021,     -27}};
+#endif
+
+
+#if THERMISTORHEATER == 1
+#define NUMTEMPS NUMTEMPS_1
+#define temptable temptable_1
+#elif THERMISTORHEATER == 2
+#define NUMTEMPS NUMTEMPS_2
+#define temptable temptable_2
+#elif THERMISTORHEATER == 3
+#define NUMTEMPS NUMTEMPS_3
+#define temptable temptable_3
+#elif THERMISTORHEATER == 4
+#define NUMTEMPS NUMTEMPS_4
+#define temptable temptable_4
+#elif THERMISTORHEATER == 5
+#define NUMTEMPS NUMTEMPS_5
+#define temptable temptable_5
+#elif defined HEATER_USES_THERMISTOR
+#error No heater thermistor table specified
+#endif
+#if THERMISTORBED == 1
+#define BNUMTEMPS NUMTEMPS_1
+#define bedtemptable temptable_1
+#elif THERMISTORBED == 2
+#define BNUMTEMPS NUMTEMPS_2
+#define bedtemptable temptable_2
+#elif THERMISTORBED == 3
+#define BNUMTEMPS NUMTEMPS_3
+#define bedtemptable temptable_3
+#elif THERMISTORBED == 4
+#define BNUMTEMPS NUMTEMPS_4
+#define bedtemptable temptable_4
+#elif THERMISTORBED == 5
+#define BNUMTEMPS NUMTEMPS_5
+#define bedtemptable temptable_5
+#elif defined BED_USES_THERMISTOR
+#error No bed thermistor table specified
+#endif
+
+#endif //THERMISTORTABLES_H_
